e2b64f8bc667f6a1789c4688c894987e8bab6d58
Jarome <jwolf@techfak.uni-bielefeld.de>
2023-09-20 17:09:31 +0200

    Added a new home page and its structure. Things develop slowly as mom doesnt write texts

--- tagespflege/src/App.vue
+++ tagespflege/src/App.vue
@@ -1,6 +1,23 @@
 <template>
     <Navbar />
     <router-view />
+    <div class="footer">
+        <div class="kontakt">
+            <ul>
+                Adresse<!--Adresse und Kontaktdaten sowie Name-->
+            </ul>
+        </div>
+        <div class="quicklinks">
+            <ul>
+                <li>links</li><!--Navbar quicklinks-->
+            </ul>
+        </div>
+        <div class="footprint">
+            <p>
+                Autor<!--Autor und publishing Lizenz-->
+            </p>
+        </div>
+    </div>
 </template>
 
 <script>
--- tagespflege/src/views/HomeView.vue
+++ tagespflege/src/views/HomeView.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="home">
-        <h1>Starting page</h1>
+        <div class="startseite">
+            <div class="intro">
+                <h1>headline</h1>
+                <p>description</p>
+                <div class="dicover_button">
+                    Erkunde
+                </div>
+            </div>
+            <div class="startseite_bild">
+                startseite Bild <!--Bild-->
+            </div>
+        </div>
+        <div class="startseite_willkommen">
+            <div class="willkommens_bild">
+                Bild willkommen
+            </div>
+            <p>Wilkommenstext</p>
+        </div>
+        <div class="startseite_streckbrief">
+            <ul>
+                <li>Mutter Steckrief</li><!--Stechbrief über Mutter mit kleiner Beschreibung-->
+            </ul>
+            <div class="steckbrief_bild">
+                Bild von Mutter<!-- Ein Bild von Mutter-->
+            </div>
+            <ul>
+                <li>
+                    Weiterer Steckbrief
+                </li><!-- Weitere Beschreibung-->
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -9,3 +39,18 @@ export default {
     name: "HomeView",
 };
 </script>
+
+<style>
+    .introductory_text {
+        text-align: left;
+        padding-right: 50%;
+        display: block;
+    }
+    .title {
+        background-color: aliceblue;
+    }
+    .descriptive_text {
+        background-color: blanchedalmond;
+        font-size: large;
+    }
+</style>
